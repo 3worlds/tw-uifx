12040be2d873d43e4f735ea186a916fe74ea5e2f
Ian Davies <u9007333@anu.edu.au>
2018-12-28 11:37:47 +1100

    upgrade to modify rev code to "+" for all intra library dependencies

--- tw-uifx/scripts/fr/cnrs/iees/versioning/VersionManager.java
+++ tw-uifx/scripts/fr/cnrs/iees/versioning/VersionManager.java
@@ -110,7 +110,7 @@ public class VersionManager {
 	private static String[][] DEPS = { 
 		{"org.controlsfx", "controlsfx", "8.40.14"},
 		{"fr.ens.biologie", "generics", "+"},
-		{"au.edu.anu.rscs.aot", "omugi", "0.0.3+"},
+		{"au.edu.anu.rscs.aot", "omugi", "+"},
 		{"fr.cnrs.iees.tw-core", "tw-core", "+"},
 		{"au.edu.anu.tw-apps", "tw-apps", "+"}
 	};
