6f2a569a370f816204fb13208d4131b74e109a91
daviesi <u9007333@anu.edu.au>
2019-01-28 12:20:58 +1100

    migration

---
+++ tw-uifx/src/au/edu/anu/twuifx/mm/propertyEditors/fileType/FileTypeEditor.java
@@ -0,0 +1,57 @@
+/**************************************************************************
+ *  TW-UIFX - ThreeWorlds User-Interface fx                               *
+ *                                                                        *
+ *  Copyright 2018: Jacques Gignoux & Ian D. Davies                       *
+ *       jacques.gignoux@upmc.fr                                          *
+ *       ian.davies@anu.edu.au                                            * 
+ *                                                                        *
+ *  TW-UIFX contains the Javafx interface for ModelMaker and ModelRunner. *
+ *  This is to separate concerns of UI implementation and the code for    *
+ *  these java programs.                                                  *
+ *                                                                        *
+ **************************************************************************                                       
+ *  This file is part of TW-UIFX (ThreeWorlds User-Interface fx).         *
+ *                                                                        *
+ *  TW-UIFX is free software: you can redistribute it and/or modify       *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  TW-UIFX is distributed in the hope that it will be useful,            *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *                         
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with TW-UIFX.                                                   *
+ *  If not, see <https://www.gnu.org/licenses/gpl.html>.                  *
+ *                                                                        *
+ **************************************************************************/
+package au.edu.anu.twuifx.mm.propertyEditors.fileType;
+
+import org.controlsfx.control.PropertySheet.Item;
+import org.controlsfx.property.editor.AbstractPropertyEditor;
+
+import javafx.beans.value.ObservableValue;
+import javafx.scene.layout.Pane;
+
+public class FileTypeEditor extends AbstractPropertyEditor<String,Pane>{
+
+	public FileTypeEditor(Item property, Pane control) {
+		super(property, control);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setValue(String value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected ObservableValue<String> getObservableValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ tw-uifx/src/au/edu/anu/twuifx/mm/propertyEditors/fileType/FileTypeItem.java
@@ -0,0 +1,95 @@
+/**************************************************************************
+ *  TW-UIFX - ThreeWorlds User-Interface fx                               *
+ *                                                                        *
+ *  Copyright 2018: Jacques Gignoux & Ian D. Davies                       *
+ *       jacques.gignoux@upmc.fr                                          *
+ *       ian.davies@anu.edu.au                                            * 
+ *                                                                        *
+ *  TW-UIFX contains the Javafx interface for ModelMaker and ModelRunner. *
+ *  This is to separate concerns of UI implementation and the code for    *
+ *  these java programs.                                                  *
+ *                                                                        *
+ **************************************************************************                                       
+ *  This file is part of TW-UIFX (ThreeWorlds User-Interface fx).         *
+ *                                                                        *
+ *  TW-UIFX is free software: you can redistribute it and/or modify       *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  TW-UIFX is distributed in the hope that it will be useful,            *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *                         
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with TW-UIFX.                                                   *
+ *  If not, see <https://www.gnu.org/licenses/gpl.html>.                  *
+ *                                                                        *
+ **************************************************************************/
+package au.edu.anu.twuifx.mm.propertyEditors.fileType;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.controlsfx.property.editor.PropertyEditor;
+
+import au.edu.anu.rscs.aot.graph.AotNode;
+import au.edu.anu.twapps.mm.GraphState;
+import au.edu.anu.twcore.specificationCheck.Checkable;
+import au.edu.anu.twuifx.mm.propertyEditors.NodeItem;
+import javafx.beans.value.ObservableValue;
+import javafx.stage.FileChooser;
+
+/**
+ * Author Ian davies
+ *
+ * Date Jan 28, 2019
+ */
+// TODO Problem how to avoid polluting tw-apps or tw-core with FileChooser which
+// is fx stage specific
+
+public class FileTypeItem extends NodeItem {
+
+	private List<FileChooser.ExtensionFilter> exts;
+
+	public FileTypeItem(String key, AotNode n, boolean canEdit, String category, Checkable checker) {
+		super(key, n, canEdit, category, checker);
+	}
+
+	public void setExtensions(List<FileChooser.ExtensionFilter> exts) {
+		this.exts = exts;
+	}
+
+	public List<FileChooser.ExtensionFilter> getExtensions() {
+		return exts;
+	}
+
+	@Override
+	public Optional<Class<? extends PropertyEditor<?>>> getPropertyEditorClass() {
+		return Optional.of(FileTypeEditor.class);
+	}
+
+	@Override
+	public Object getValue() {
+		FileType ft = (FileType) n.getPropertyValue(key);
+		return ft.getRelativePath();
+	}
+
+	@Override
+	public void setValue(Object newValue) {
+		Object oldValue = getValue();
+		if (!oldValue.toString().equals(newValue.toString())) {
+			FileType ft = (FileType) n.getPropertyValue(key);
+			ft.setRelativePath((String) newValue);
+			GraphState.isChanged(true);
+			checker.validateGraph();
+		}
+	}
+
+	@Override
+	public Optional<ObservableValue<? extends Object>> getObservableValue() {
+		return Optional.empty();
+	}
+
+}
