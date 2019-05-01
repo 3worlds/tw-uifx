c711564b80ee8a6a627000ce29e7579d693099dc
daviesi <u9007333@anu.edu.au>
2019-05-01 11:37:46 +1000

    issue #1

--- tw-uifx/src/au/edu/anu/twuifx/mm/visualise/TreeColours.java
+++ tw-uifx/src/au/edu/anu/twuifx/mm/visualise/TreeColours.java
@@ -8,8 +8,8 @@ import javafx.scene.paint.Color;
 public class TreeColours implements Configuration{
 	private static Map<String,Color> nodeColours= new HashMap<>();
 	static {
-		nodeColours.put(N_ECOLOGY, Color.CHARTREUSE);
-		nodeColours.put(N_CODESOURCE, Color.LIGHTGREY);
+		nodeColours.put(N_SYSTEM, Color.CHARTREUSE);
+		nodeColours.put(N_DATADEFINITION, Color.LIGHTGREY);
 		nodeColours.put(N_DATAIO, Color.SKYBLUE);
 		nodeColours.put(N_EXPERIMENT, Color.GOLDENROD);
 		nodeColours.put(N_UI, Color.THISTLE);
