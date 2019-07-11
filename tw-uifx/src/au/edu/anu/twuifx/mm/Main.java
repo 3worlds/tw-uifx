0ca4d0ce79871a3c96e6999f667a6355aad5f242
Jacques Gignoux <jacques.gignoux@upmc.fr>
2019-07-11 11:04:25 +0200

    fixing javafx problems

---
+++ tw-uifx/src/au/edu/anu/twuifx/mm/Main.java
@@ -0,0 +1,11 @@
+package au.edu.anu.twuifx.mm;
+
+import javafx.application.Application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Application.launch(ModelMakerfx.class);
+	}
+
+}
