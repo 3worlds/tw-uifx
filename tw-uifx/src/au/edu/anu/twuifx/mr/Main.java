fc4c2880e71c608405fa67bd4a90dac88b638d51
Ian Davies <ian.davies@anu.edu.au>
2019-08-30 15:31:37 +1000

    cleaning up

--- tw-uifx/src/au/edu/anu/twuifx/mr/Main.java
+++ tw-uifx/src/au/edu/anu/twuifx/mr/Main.java
@@ -50,20 +50,20 @@ public class Main {
 			System.exit(1);
 		}
 		Project.open(prjDir);
-		File userJar = Project.makeFile(Project.getProjectUserName() + ".jar");
-		if (!userJar.exists()) {
-			System.out.println("User generated classes not found: [" + userJar + "]");
-			System.exit(1);
 
+		// If we are not running from a jar the load the generated classes
+		if (Jars.getRunningJarFilePath(Main.class) == null) {
+			File userJar = Project.makeFile(Project.getProjectUserName() + ".jar");
+			if (!userJar.exists()) {
+				System.out.println("User generated classes not found: [" + userJar + "]");
+				System.exit(1);
+			}
+			OmugiClassLoader.setJarClassLoader(userJar);
+		} else {
+			// we are running from a jar so all classes are here.
 		}
-
-		if (Jars.getRunningJarFilePath(Main.class) == null) 
-			loadUserClasses(userJar);
 		TreeGraph<TreeGraphNode, ALEdge> configGraph = (TreeGraph<TreeGraphNode, ALEdge>) FileImporter
 				.loadGraphFromFile(Project.makeConfigurationFile());
-		// TwConfigFactory labels are lost somewhere???
-//		SimulationSession session = new SimulationSession(configGraph);
-		// generated classes are not accessible here?
 		List<Initialisable> initList = new LinkedList<>();
 		for (TreeGraphNode n : configGraph.nodes())
 			initList.add((Initialisable) n);
@@ -77,69 +77,12 @@ public class Main {
 			TreeGraphNode uiNode = (TreeGraphNode) get(configGraph.root().getChildren(),
 					selectZeroOrOne(hasTheLabel(N_UI.label())));
 			if (uiNode != null) {
+				// ok now we can start building the ui
 				System.out.println("Ready to launch UI");
 			} else {
 				System.out.println("Ready to run without UI?");
 			}
 		}
-	}
-
-	private static void loadUserClasses(File userJar) {
-<<<<<<< HEAD
-		URL userUrl;
-		try {
-			userUrl = userJar.toURI().toURL();
-			URL path[] = { userUrl };
-			ClassLoader parent = ClassLoader.getPlatformClassLoader();
-			URLClassLoader child = new URLClassLoader(path, parent);
-			OmugiClassLoader.setClassLoader(child);
-			// test code
-			try {
-				Class<? extends TwFunction> functionClass = (Class<? extends TwFunction>) Class
-						.forName("system1.Function1", false, child);
-				System.out.println(functionClass);
-				// ok to here
-				Constructor<? extends TwFunction> nodeConstructor = functionClass.getConstructor();
-				System.out.println(nodeConstructor);
-				Object function = nodeConstructor.newInstance();
-				System.out.println(function.getClass());
-				System.out.println(function.getClass().getSuperclass());
-				System.out.println(function.getClass().getSuperclass().getSuperclass());
-				System.out.println(function.getClass().getSuperclass().getSuperclass().getSuperclass());
-				TwFunction f = new MyChangeStateFunction();
-		/*
-				 * fail here. class system1.Function1 cannot be cast to class
-				 * au.edu.anu.twcore.ecosystem.runtime.TwFunction (system1.Function1 is in
-				 * unnamed module of loader java.net.URLClassLoader @17c68925;
-				 * au.edu.anu.twcore.ecosystem.runtime.TwFunction is in unnamed module of loader
-				 * 'app')
-				 * 
-				 * humm... class hierarchy spans two classLoaders
-				 * i.e. we are looking at two different class defs of TwFunction: one here in eclispe in the app classLoader and one in the 
-				 * twdep.jar
-				 */
-				f = (TwFunction) function;
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			//
-
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-//		String pathSeparator = System
-//			    .getProperty("path.separator");
-//			String[] classPathEntries = System
-//			    .getProperty("java.class.path")
-//			    .split(pathSeparator);
-//			for (String s: classPathEntries) {
-//				System.out.println(s);
-//			}
-
-=======
-		OmugiClassLoader.setJarClassLoader(userJar);
->>>>>>> branch 'master' of git@gitlab.anu.edu.au:ThreeWorlds/tw-uifx.git
 	}
 
 }
