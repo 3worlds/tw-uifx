a253ba6a85f03557632448590b0fe4d6b6f824a6
Ian Davies <ian.davies@anu.edu.au>
2019-09-04 11:43:20 +1000

    version typo

--- tw-uifx/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ tw-uifx/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -60,7 +60,7 @@ public class VersionSettings {
 		{"au.edu.anu.rscs.aot", "aot", "[0.1.2,)", null},
 		{"au.edu.anu.tw-apps", "tw-apps", "[0.1.1,)", null},
 		{"au.edu.anu.rscs.aot", "qgraph", "[0.1.1,)", null},	
-		{"au.edu.anu.rscs.ymuit", "ymuit", "[0.1.1,)", null},	
+		{"au.edu.anu.ymuit", "ymuit", "[0.1.1,)", null},	
 		{"org.openjfx", "javafx-fxml", "11+", "_os"},
 		{"org.openjfx", "javafx-controls", "11+", "_os"},
 		{"org.openjfx", "javafx-graphics", "11+", "_os"},
