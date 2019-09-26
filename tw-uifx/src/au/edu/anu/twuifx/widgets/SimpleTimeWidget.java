8cfa47428ec1f1e7902225b2c61cd58b7e05714d
Ian Davies <ian.davies@anu.edu.au>
2019-09-27 07:03:17 +1000

    TimeLine origin must be 1..1

--- tw-uifx/src/au/edu/anu/twuifx/widgets/SimpleTimeWidget.java
+++ tw-uifx/src/au/edu/anu/twuifx/widgets/SimpleTimeWidget.java
@@ -96,7 +96,7 @@ public class SimpleTimeWidget extends AbstractDisplayWidget<TimeData, Metadata> 
 
 	private void processOnDataMessage(TimeData data) {
 		log.info("Thread id: " + Thread.currentThread().getId());
-		lblTime.setText("(" + policy.sender() + ")" + timeFormatter.getTimeText(data.time()));
+		lblTime.setText("(" + policy.sender() + ") " + timeFormatter.getTimeText(data.time()));
 	}
 
 	@Override
@@ -105,7 +105,7 @@ public class SimpleTimeWidget extends AbstractDisplayWidget<TimeData, Metadata> 
 		HBox content = new HBox();
 		content.setPadding(new Insets(5, 1, 1, 2));
 		content.setSpacing(5);
-		lblTime = new Label("uninitialised time origin");
+		lblTime = new Label("(" + policy.sender() + ")" + timeFormatter.getTimeText(timeFormatter.getInitialTime()));
 		content.getChildren().addAll(lblTime);
 		return content;
 	}
