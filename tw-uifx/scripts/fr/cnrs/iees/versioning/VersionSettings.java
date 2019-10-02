0fa6b5c209faf9f79addec6ca103bef2367145f8
Jacques Gignoux <jacques.gignoux@upmc.fr>
2019-10-02 09:21:25 +0200

    versioning

--- tw-uifx/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ tw-uifx/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -55,7 +55,7 @@ public class VersionSettings {
 	 */
 	protected static String[][] DEPS = { 
 		{"fr.ens.biologie", "generics", "[0.1.12,)", null},
-		{"au.edu.anu.rscs.aot", "omugi", "[0.2.13,)", null},
+		{"au.edu.anu.rscs.aot", "omugi", "[0.2.14,)", null},
 		{"fr.cnrs.iees.tw-core", "tw-core", "[0.3.1,)", null},
 		{"au.edu.anu.rscs.aot", "aot", "[0.1.6,)", null},
 		{"au.edu.anu.tw-apps", "tw-apps", "[0.2.1,)", null},
