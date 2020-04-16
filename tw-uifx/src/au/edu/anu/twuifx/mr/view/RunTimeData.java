9c69a3bf66a3dad2f269e3306406bbb5950e5aba
Jacques Gignoux <jacques.gignoux@upmc.fr>
2020-04-16 17:14:02 +0200

    consequences of refactoring in twcore

--- tw-uifx/src/au/edu/anu/twuifx/mr/view/RunTimeData.java
+++ tw-uifx/src/au/edu/anu/twuifx/mr/view/RunTimeData.java
@@ -3,8 +3,9 @@ package au.edu.anu.twuifx.mr.view;
 import au.edu.anu.twcore.data.runtime.TwData;
 import au.edu.anu.twcore.ecosystem.dynamics.SimulatorNode;
 import au.edu.anu.twcore.ecosystem.runtime.simulator.Simulator;
-import au.edu.anu.twcore.ecosystem.runtime.containers.CategorizedContainer;
+
 import au.edu.anu.twcore.ecosystem.runtime.system.SystemComponent;
+import au.edu.anu.twcore.ecosystem.runtime.system.CategorizedContainer;
 import au.edu.anu.twcore.ecosystem.runtime.system.ComponentContainer;
 import au.edu.anu.twcore.ecosystem.runtime.system.SystemFactory;
 import au.edu.anu.twcore.ecosystem.structure.ComponentType;
@@ -31,13 +32,13 @@ import static au.edu.anu.rscs.aot.queries.CoreQueries.*;
  * drivers/state. If a new state is generated, will there be new parameters. If
  * so, modification of initial state files would invalidate parameter files. Is
  * this possible???
- * 
+ *
  * If so, drivers will have to be saved to the same file which will confound
  * debugging models.
- * 
+ *
  * For me: Parameter files contain properties where nothing can change apart
  * from editing the property values (constrained by meta-data).
- * 
+ *
  * Driver files contain ONLY things that change - populations, property values
  * and the state of random number streams. BTW This is violated if rns are hard
  * coded. Therefore, rns must appear in the graph for their state to be saved
@@ -62,7 +63,7 @@ public class RunTimeData {
 		 * at runTime and use various tools to create large tables. These should be a
 		 * copy so changes don't take place unless an "Apply" button is pressed? Apply
 		 * would call putParameters()
-		 * 
+		 *
 		 * The return graph can be saved.
 		 */
 
@@ -105,12 +106,12 @@ public class RunTimeData {
 	 * populations
 	 */
 	public static void clearModelState(TreeGraph<TreeGraphDataNode, ALEdge> initialisedConfig) {
-		for (ComponentContainer community : communities(initialisedConfig)) 
+		for (ComponentContainer community : communities(initialisedConfig))
 			community.clearState();
 	}
-	
+
 	public static void resetModelState(TreeGraph<TreeGraphDataNode, ALEdge> initialisedConfig) {
-		for (ComponentContainer community : communities(initialisedConfig)) 
+		for (ComponentContainer community : communities(initialisedConfig))
 			community.reset();
 	}
 
@@ -137,7 +138,7 @@ public class RunTimeData {
 		 * Purpose: Return all necessary things so driver data and populations can be
 		 * created with some tool for bootstrapping a model. The output of this tool
 		 * will be a state graph to be saved.
-		 * 
+		 *
 		 * Issues: If this process creates parameters, then everyting is confounded?
 		 */
 		return null;
@@ -146,10 +147,10 @@ public class RunTimeData {
 	// ------------------------------------------------
 
 //	JG:
-//	All parameters are stored in SystemContainers, organised as a tree 
-//	(cf class CategorizedContainer), through the CategorizedContainer.parameters() method. 
+//	All parameters are stored in SystemContainers, organised as a tree
+//	(cf class CategorizedContainer), through the CategorizedContainer.parameters() method.
 //	There is a separate tree instance for every Simulator instance, accessible through
-//	the Simulator.community() method. The Simulator instances are accessible through 
+//	the Simulator.community() method. The Simulator instances are accessible through
 //	SimulatorNode.getInstance(id), which is in the initialedConfig tree passed
 //	as argument here. But you also need to know the id of your current simulator
 //	instance (eg the one being currently running in simple cases, or if many
@@ -208,7 +209,7 @@ public class RunTimeData {
 
 	private static void printContainer(ComponentContainer container) {
 		System.out.println("ID: " + container.id());
-		System.out.println("VARS:" + container.variables());
+//		System.out.println("VARS:" + container.variables());
 		System.out.println("PARS:" + container.parameters());
 		if (container.parameters()!=null) {
 			TwData pars = container.parameters().clone();
@@ -221,7 +222,7 @@ public class RunTimeData {
 			SimplePropertyList props =  component.properties().clone();
 			for (String key:props.getKeysAsSet())
 				System.out.println(key+":"+props.getPropertyValue(key));
-			
+
 		}
 		for (CategorizedContainer<SystemComponent> childContainer : container.subContainers()) {
 			printContainer((ComponentContainer) childContainer);
