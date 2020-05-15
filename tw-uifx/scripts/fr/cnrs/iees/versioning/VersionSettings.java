776649ea1f9dda259117fe8b14dbf7ccd05919ea
Ian Davies <ian.davies@anu.edu.au>
2020-05-16 06:44:47 +1000

    dependency typo

--- tw-uifx/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ tw-uifx/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -57,7 +57,7 @@ public class VersionSettings {
 		{"fr.ens.biologie", "generics", "[0.1.12,)", null},
 		{"au.edu.anu.rscs.aot", "omugi", "[0.2.14,)", null},
 		{"fr.cnrs.iees.tw-core", "tw-core", "[0.3.17,)", null},
-		{"au.edu.anu.rscs.aot", "aot", "[0.1.7,)", null},
+		{"au.edu.anu.rscs.aot", "aot", "[0.1.8,)", null},
 		{"au.edu.anu.tw-apps", "tw-apps", "[0.2.1,)", null},
 		{"au.edu.anu.rscs.aot", "qgraph", "[0.1.3,)", null},
 		{"au.edu.anu.ymuit", "ymuit", "[0.1.6,)", null},
