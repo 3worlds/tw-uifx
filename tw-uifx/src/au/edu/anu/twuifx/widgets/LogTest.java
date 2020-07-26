bfdf97605e2dcefe1f7b67ec33e0881b2d7778f7
Ian Davies <ian.davies@anu.edu.au>
2020-07-26 13:51:22 +1000

    only have one logger service provider

--- tw-uifx/src/au/edu/anu/twuifx/widgets/LogTest.java
+++ tw-uifx/src/au/edu/anu/twuifx/widgets/LogTest.java
@@ -1,9 +1,11 @@
 package au.edu.anu.twuifx.widgets;
+import java.lang.invoke.MethodHandles;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LogTest {
-	private static final Logger log = LoggerFactory.getLogger(LogTest.class);
+	private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	public static void main(String[] args) {
 		log.info("Log test");
 		
