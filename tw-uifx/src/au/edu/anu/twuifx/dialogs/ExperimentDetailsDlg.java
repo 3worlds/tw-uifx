c4fb9da7ac2e4ab77c4c4036efd57b2f93a68742
Ian Davies <ian.davies@anu.edu.au>
2021-07-21 17:07:55 +1000

    experiment details dlg

--- tw-uifx/src/au/edu/anu/twuifx/dialogs/ExperimentDetailsDlg.java
+++ tw-uifx/src/au/edu/anu/twuifx/dialogs/ExperimentDetailsDlg.java
@@ -3,14 +3,20 @@ package au.edu.anu.twuifx.dialogs;
 import java.util.List;
 import java.util.Optional;
 
+import au.edu.anu.rscs.aot.collections.tables.StringTable;
 import au.edu.anu.rscs.aot.graph.property.Property;
 import au.edu.anu.twapps.dialogs.Dialogs;
 import au.edu.anu.twcore.experiment.Design;
 import au.edu.anu.twcore.experiment.Experiment;
+import au.edu.anu.twcore.experiment.Treatment;
+import fr.cnrs.iees.graph.Direction;
+import fr.cnrs.iees.graph.impl.ALDataEdge;
 import fr.cnrs.iees.graph.impl.ALEdge;
 import fr.cnrs.iees.graph.impl.TreeGraph;
 import fr.cnrs.iees.graph.impl.TreeGraphDataNode;
 import fr.cnrs.iees.twcore.constants.ExperimentDesignType;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
@@ -26,8 +32,8 @@ import javafx.scene.control.ButtonBar.ButtonData;
 import static au.edu.anu.rscs.aot.queries.CoreQueries.*;
 import static au.edu.anu.rscs.aot.queries.base.SequenceQuery.get;
 import static fr.cnrs.iees.twcore.constants.ConfigurationNodeLabels.*;
-import static fr.cnrs.iees.twcore.constants.ConfigurationPropertyNames.P_DESIGN_TYPE;
-import static fr.cnrs.iees.twcore.constants.ConfigurationPropertyNames.P_EXP_NREPLICATES;
+import static fr.cnrs.iees.twcore.constants.ConfigurationEdgeLabels.*;
+import static fr.cnrs.iees.twcore.constants.ConfigurationPropertyNames.*;
 
 public class ExperimentDetailsDlg {
 	public ExperimentDetailsDlg(TreeGraph<TreeGraphDataNode, ALEdge> g) {
@@ -55,65 +61,61 @@ public class ExperimentDetailsDlg {
 		BorderPane borderPane = new BorderPane();
 		dlg.getDialogPane().setContent(borderPane);
 		ScrollPane scrollPane = new ScrollPane();
+		scrollPane.setPrefSize(400, 300);
 		borderPane.setCenter(scrollPane);
 		GridPane gridPane = new GridPane();
 		scrollPane.setContent(gridPane);
-//		gridPane.setVgap(15);
 		gridPane.setHgap(10);
 
 		// col,row
-		TextField tf;
-		gridPane.add(new Label("Type"), 0, 0);
-		tf = new TextField(expType);
-		tf.setEditable(false);
-		gridPane.add(tf, 1, 0);
-		gridPane.add(new Label("Description"), 0, 1);
-		tf = new TextField(expDesc);
-//		tf.setMinWidth(Region.USE_PREF_SIZE);
-		tf.setPrefWidth(400);
-		tf.setEditable(false);
-		gridPane.add(tf, 1, 1);
-		gridPane.add(new Label("Replicates"), 0, 2);
-		tf = new TextField(Integer.toString(nReps));
-		tf.setEditable(false);
-		gridPane.add(tf, 1, 2);
+		int col = 0;
+		int row = 0;
+		addLine("Type:", expType, row++, col, gridPane);
+		addLine("Description:", expDesc, row++, col, gridPane);
+		if (edt != null && edt.equals(edt.singleRun)) {
+			addLine("Replicates:", Integer.toString(nReps), row++, col, gridPane);
+			if (nReps > 1)
+				addLine("Deployment: ", "parallel", row++, col, gridPane);
+		}
 		if (edt != null && edt.equals(edt.crossFactorial)) {
-			gridPane.add(new Label("Simulator"), 0, 3);
-			gridPane.add(new Label("Factors"), 1, 3);
 			List<List<Property>> lst = Experiment.buildSimpleFactorialTreatmentList(exp);
+			Treatment treatment = (Treatment) get(exp.getChildren(),selectOne(hasTheLabel(N_TREATMENT.label())));
+			List<ALDataEdge> treatments = (List<ALDataEdge>) get(treatment.edges(Direction.OUT),
+					selectOneOrMany(hasTheLabel(E_TREATS.label())));
+			StringBuilder sb = new StringBuilder().append("rep(").append(nReps).append(")");
+			int total = nReps;
+			for (ALDataEdge e : treatments) {
+				StringTable tbl = (StringTable) e.properties().getPropertyValue(P_TREAT_VALUES.key());
+				String p = e.endNode().id();
+				sb.append(" x ").append(e.endNode().id()).append("(").append(tbl.size()).append(")");
+				total*=tbl.size();
+			}
+			sb.append("=").append(" total runs(").append(total).append(")");
+			addLine("Design:",sb.toString(),row++,col,gridPane);
+			addLine("Simulator:", "Factors:", row++, col, gridPane);
 			int sim = 0;
 			for (int r = 0; r < nReps; r++)
 				for (List<Property> factors : lst) {
 					String f = factors.toString().replaceAll("Property:", "");
 					f = f.replace("[[", "");
 					f = f.replace("]]", "");
-					f = f.replace("]","");
-					f = f.replace("[","");
-					gridPane.add(new Label(Integer.toString(sim)), 0, sim+4);
-					tf = new TextField(f);
-					tf.setEditable(false);
-
-//					tf.setPrefWidth(1000);
-					gridPane.add(tf, 1, sim+4);
-					sim++;
+					f = f.replace("]", "");
+					f = f.replace("[", "");
+					addLine(Integer.toString(sim++), f, row++, col, gridPane);
 				}
 		}
-//		for (int c = 0; c < gridPane.getColumnCount(); c++) {
-//			ColumnConstraints cc = new ColumnConstraints();
-//			if (c==gridPane.getColumnCount()-1) {
-//				cc.setFillWidth(true);
-//				cc.setHgrow(Priority.ALWAYS);
-//			
-//			}else {
-//				cc.setFillWidth(false);
-//				cc.setHgrow(Priority.NEVER);
-//			}
-//			gridPane.getColumnConstraints().add(cc);
-//
-//		}
-
-//		Optional<ButtonType> result = dlg.showAndWait();
 		dlg.show();
+
+	}
+
+	private static void addLine(String desc, String value, int row, int col, GridPane grid) {
+		Label lblDesc = new Label(desc);
+		Label lblItem = new Label(value);
+		grid.add(lblDesc, col, row);
+		grid.add(lblItem, col + 1, row);
+		GridPane.setHalignment(lblDesc, HPos.RIGHT);
+		GridPane.setHalignment(lblItem, HPos.LEFT);
+		GridPane.setValignment(lblItem, VPos.CENTER);
 
 	}
 
